58f3540368a942179c034dd5cc9590872f2bad6a
shariarratul <alshariarratul@gmail.com>
2024-07-10 23:58:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
-from flask import Flask, request, jsonify, send_file
-import yt_dlp as youtube_dl
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
+import yt_dlp
 
 app = Flask(__name__)
-CORS(app)  # This will enable CORS for all routes
+CORS(app)
 
 @app.route('/download', methods=['POST'])
 def download_video():
-    data = request.json
+    data = request.get_json()
     url = data.get('url')
     resolution = data.get('resolution')
-
+    
     ydl_opts = {
         'format': f'best[height<={resolution}]',
-        'outtmpl': 'downloaded_video.%(ext)s',
+        'outtmpl': 'downloads/%(title)s.%(ext)s'
     }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+        video_file = ydl.prepare_filename(info_dict)
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    return jsonify({'file_url': f'/{video_file}'})
 
-    return jsonify({'file_url': '/downloaded_video.mp4'})
-
-@app.route('/downloaded_video.mp4')
-def get_video():
-    return send_file('downloaded_video.mp4')
+@app.route('/downloads/<path:filename>', methods=['GET'])
+def download_file(filename):
+    return send_from_directory('downloads', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+    app.run(debug=True)
